d8befd3ac1fe7f5e10169fa2123e994c6a12c54b
Sistem Pemesanan Makanan Online <SisPesanMakanan@users.noreply.github.com>
2015-07-11 08:04:24 +0700

    Delete clsDataProduk.java

--- src/java/pilar/data/clsDataProduk.java
+++
@@ -1,9 +0,0 @@
-package pilar.data;
-
-
-public class clsDataProduk {
-    public static String[] vPubSKode = {"P001","P002","P003","P004","P005","P006","P007","P008","P009","P010"};
-    public static String[] vPubSNama = {"Aqua","Buavita","Caprisone","Milo","Pocari","Silver Queen","Selai Kacang","Apel Washington","Chitato","Kacang Garuda"};
-    public static String[] vPubSHarga = {"1500","2000","2500","3000","3500","1500","2000","2500","3000","3500"}; 
-    public static String[] vPubSSatuan = {"btl","ktk","ktk","ktk","btl","btg","btl","kg","bks","bks"};
-}
